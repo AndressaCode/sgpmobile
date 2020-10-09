832e156e6f8e3db48aa9bb98f2d63520e8245e9a
Andressa Silva <developerandressa@gmail.com>
2020-10-09 01:16:32 -0300

    Implementação da página inicial

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,8 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
+    final _user = new TextEditingController();
+
     return Scaffold(
 //      appBar: AppBar(
 //        title: Text('Central de Tickets Mobile',
@@ -26,30 +28,70 @@ class _HomeState extends State<Home> {
               width: MediaQuery.of(context).size.height * 4,
               height: 200,
               decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Colors.deepOrange,
-                    Colors.deepOrange,
-                  ]
-                )
-              ),
+                  gradient: LinearGradient(colors: [
+                Color(0xFF64718C),
+                Color(0xFF64718C),
+              ])),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Icon(Icons.airplay, size: 65, color: Colors.white),
-                  SizedBox(height: 12),
-                  Text('Colocar logo aqui',
-                    textAlign: TextAlign.right,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 32,
-                      fontFamily: 'Roboto',),
+                  Center(
+                    child: Container(
+                      width: 400,
+                      height: 200.0,
+                      child: Image(
+                        image: AssetImage('assets/sgp-logo.png'),
+                      ),
+                    ),
                   ),
                 ],
               ),
             ),
-            SizedBox(height: 40.0,),
-            Container(),
+            Divider(
+              color: Colors.black,
+              thickness: 20.0,
+            ),
+            SizedBox(
+              height: 40.0,
+            ),
+            Text(
+              'Central do assinante',
+              style: TextStyle(
+                  color: Color(0xFF64718C),
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0,
+                  fontStyle: FontStyle.italic),
+            ),
+            Divider(
+              color: Color(0xFF64718C),
+              height: 20.0,
+              thickness: 5.0,
+              indent: 20.0,
+              endIndent: 20.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text('Aqui vai o switcher Login/Senha ou CPF/CNPJ'),
+            ),
+            Divider(
+              color: Color(0xFF64718C),
+              height: 40.0,
+              thickness: 5.0,
+              indent: 20.0,
+              endIndent: 20.0,
+            ),
+            Container(
+              child: TextFormField(
+                autofocus: true,
+                keyboardType: TextInputType.text,
+                controller: _user,
+                style:
+                new TextStyle(color: Colors.deepPurple, fontSize: 22),
+                decoration: InputDecoration(
+                  labelText: "Matricula",
+                ),
+              ),
+            ),
           ],
         ),
       ),
